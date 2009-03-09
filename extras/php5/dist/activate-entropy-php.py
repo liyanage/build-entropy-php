bd17ee75d7535a41c975fd47d4c8be7ebc139e6b
Marc Liyanage <reg.gitcommit@entropy.ch>
2009-03-09 17:14:19 +0100

    postflight script update for Leopard / Apache2

--- extras/php5/dist/activate-entropy-php.py
+++ extras/php5/dist/activate-entropy-php.py
@@ -11,16 +11,11 @@ prefix = '{prefix}'
 #
 httpd_conf_path = prefix + '/entropy-php.conf'
 
-# test is reversed, shell exit code true/false values
-if os.system("sw_vers | grep -q Server"):
-	httpd_conf_symlink = '/etc/httpd/users/+entropy-php.conf'
-else:
-	httpd_conf_symlink = '/etc/httpd/sites/+entropy-php.conf'
+httpd_conf_symlink = '/etc/apache2/other/+entropy-php.conf'
 
 if os.system("test -h " + httpd_conf_symlink):
 	os.symlink(httpd_conf_path, httpd_conf_symlink)
 	
-
 # Activate default php.ini and pear.conf files if they don't exist.
 # This prevents upgrade installs of the packge from clobbering local modifications
 os.system("cd " + prefix + "/lib && test -e php.ini || cp php.ini-recommended php.ini")
@@ -29,14 +24,3 @@ os.system("cd " + prefix + "/etc && test -e pear.conf || cp pear.conf.default pe
 result = os.system("apachectl restart") >> 8
 sys.exit(result)
 
-
-#extensions_path = prefix + '/lib/php/extensions/no-debug-non-zts-20050922'
-
-# for item in os.listdir(extensions_path):
-# 	cmd = "lipo -info " + extensions_path + "/" + item + "|grep -q `arch` > /dev/null"
-# 	comment = ['#', ''][os.system(cmd) == 0]
-# 	cmd = "echo '%sextension=%s' > %s/php.d/extension-%s.ini " % (comment, item, prefix, item)
-# 	print cmd
-# 	os.system(cmd)
-	
-	
