a6162a43324712473b88a2f4260a5db444fbe46b
Marc Liyanage <reg.gitcommit@entropy.ch>
2009-03-10 10:55:55 +0100

    cannot use Test::More the way I did, fall back to simpler non-OO test script

--- test/run-test.pl
+++ test/run-test.pl
@@ -3,21 +3,33 @@
 use strict;
 use warnings;
 use IO::Dir;
+use LWP::UserAgent;
+
+use Test::More tests => 2;
+
+my %conf = @ARGV;
+my $content;
+
+$content = get_url_success('test-phpinfo.php');
+($content) = $content =~ /this machine runs: (\w+)/;
+is($content, $conf{current_test_arch}, "current_test_arch");
+
+$content = get_url_success('test-mcrypt.php');
+is($content, "This is very important data\n", 'mcrypt');
 
 
-my ($basedir) = grep {-d} map {"$_/EntropyPHPTest"} @INC;
-die unless $basedir;
-
-my @testclasses = map {"EntropyPHPTest::$_"} map {/(.+)\.pm$/} IO::Dir->new($basedir)->read();
-
-foreach my $class (@testclasses) {
-	print "Running test class $class\n";
-	eval "use $class";
-	die if $@;
-	eval {
-		$class->run_test(@ARGV);
-	};
-	if ($@) {
-		warn "Test $class failed: $@\n";
-	}
+sub ua {
+	return LWP::UserAgent->new();
 }
+
+
+sub get_url_success {
+	my ($path) = @_;
+	my $url = "$conf{base_url}/$path";
+	my $response = ua()->get($url);
+	if (!$response->is_success()) {
+		die "Request to '$url' failed: " . $response->status_line();
+	}
+	return $response->decoded_content();
+}
+
--- test/t/arch.t
+++
@@ -1,6 +0,0 @@
-
-use strict;
-use warnings;
-
-use LWP::UserAgent;
-
