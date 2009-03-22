a9ec0260778da2329260a89be0766d0457f34a6f
Marc Liyanage <reg.gitcommit@entropy.ch>
2009-03-22 21:34:20 +0100

    php preinstall check

--- extras/php5/distribution-package/resources/check_for_activated_php.sh
+++ extras/php5/distribution-package/resources/check_for_activated_php.sh
@@ -1,2 +1,25 @@
 #!/bin/sh
-! egrep -qi '^[[:space:]]*LoadModule.*php' /etc/apache2/httpd.conf
+
+
+if sw_vers -productName | grep -qi server; then
+	SYMLINK=/etc/apache2/sites/+entropy-php.conf
+else
+	SYMLINK=/etc/apache2/other/+entropy-php.conf
+fi
+
+TARGET=$(readlink $SYMLINK)
+
+if [ $TARGET ]; then
+	echo temporarily deactivating entropy PHP conf
+	rm $SYMLINK
+fi
+/usr/sbin/httpd -M 2>&1 | grep -q php5_module
+#egrep -qi '^[[:space:]]*LoadModule.*php' /etc/apache2/httpd.conf
+RESULT=$?
+echo $RESULT > /tmp/entropy-check_for_activated_php_result
+
+if [ $TARGET ]; then
+	echo restoring entropy PHP conf
+	ln -s "$TARGET" $SYMLINK
+fi
+exit $RESULT
