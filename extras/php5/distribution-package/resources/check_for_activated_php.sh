aa53a6a364f6308e83323d6eda229143d9260a96
Marc Liyanage <reg.gitcommit@entropy.ch>
2009-03-22 21:48:32 +0100

    robustness fixes for preinstall php module check script

--- extras/php5/distribution-package/resources/check_for_activated_php.sh
+++ extras/php5/distribution-package/resources/check_for_activated_php.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-
+# 
+# Figure out if another PHP module is already active before
+# attempting to install the Entropy PHP distribution.
+#
+# Exits with exit code
+# - 0 (true) if another module is active
+# - 1 (false) otherwise
+#
 
 if sw_vers -productName | grep -qi server; then
 	SYMLINK=/etc/apache2/sites/+entropy-php.conf
@@ -9,17 +16,18 @@ fi
 
 TARGET=$(readlink $SYMLINK)
 
-if [ $TARGET ]; then
+if [ "$TARGET" ]; then
 	echo temporarily deactivating entropy PHP conf
 	rm $SYMLINK
 fi
+
 /usr/sbin/httpd -M 2>&1 | grep -q php5_module
-#egrep -qi '^[[:space:]]*LoadModule.*php' /etc/apache2/httpd.conf
 RESULT=$?
 echo $RESULT > /tmp/entropy-check_for_activated_php_result
 
-if [ $TARGET ]; then
+if [ "$TARGET" ]; then
 	echo restoring entropy PHP conf
 	ln -s "$TARGET" $SYMLINK
 fi
+
 exit $RESULT
